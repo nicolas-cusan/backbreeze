1a10382a88b07d43961ea7a73234c96211e067aa
Nicolas Cusan <nc@arillo.ch>
2019-10-24 18:02:11 +0200

    add more props

--- scripts/sassdoc.js
+++ scripts/sassdoc.js
@@ -1,6 +1,6 @@
 const sassdoc = require('sassdoc');
 const fs = require('fs');
 
-sassdoc.parse('./src', { verbose: true }).then(data => {
+const files = sassdoc.parse('./src', { verbose: true }).then(data => {
   fs.writeFileSync('./dist/parsed-data.json', JSON.stringify(data, null, 2));
 });
